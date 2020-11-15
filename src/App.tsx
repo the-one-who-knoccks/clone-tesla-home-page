e88fd374bce7a22f5b30bba251f0147060ea038d
Thiago Alves <the-one-who-knoccks@pop-os.localdomain>
2020-11-15 00:35:54 -0300

    :recycle: Refactored code

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
+import React from 'react';
+import GlobalStyle from './styles/GlobalStyles';
+import Page from './components/Page';
+
+function App() {
+  return (
+    <>
+      <Page />
+      <GlobalStyle />
+    </>
+  );
+}
+
+export default App;
